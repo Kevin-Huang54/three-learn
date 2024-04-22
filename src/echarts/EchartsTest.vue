b02594a83d14236b04d51287adf6d5c54722a111
Kevin <huangjunjia.sjtu@gmail.com>
2024-04-22 10:31:22 +0800

    测试echarts

--- src/echarts/EchartsTest.vue
+++ src/echarts/EchartsTest.vue
@@ -1,33 +1,58 @@
 <script setup>
 import * as echarts from 'echarts'
 import { onMounted, ref } from 'vue'
+import rawData from './data.json'
 
 const chartContainer = ref()
 
 onMounted(() => {
+  console.log(rawData)
   const myChart = echarts.init(chartContainer.value)
   // 监听窗口resize
   window.addEventListener('resize', function () {
     myChart.resize()
   })
-  const option = {
-    tooltip: {},
-    legend: {},
-    dataset: {
-      // 提供一份数据。
-      source: [
-        [43.3, 85.8, 93.7, 93.7],
-        [83.1, 73.4, 55.1, 55.1],
-        [86.4, 65.2, 82.5, 82.5],
-        [72.4, 53.9, 39.1, 39.1]
-      ]
-    },
-    // 声明一个 X 轴，类目轴（category）。默认情况下，类目轴对应到 dataset 第一列。
-    xAxis: {}, // 声明一个 Y 轴，数值轴。
-    yAxis: {},
-    // 声明多个 bar 系列，默认情况下，每个系列会自动对应到 dataset 的每一列。
-    series: [{ type: 'bar' }, { type: 'bar' }, { type: 'bar' }]
+  let base = +new Date(1968, 9, 3)
+  let oneDay = 24 * 3600 * 1000
+  let date = []
+  let data = [Math.random() * 300]
+  for (let i = 1; i < 20000; i++) {
+    const now = new Date((base += oneDay))
+    date.push([now.getFullYear(), now.getMonth() + 1, now.getDate()].join('/'))
+    data.push(Math.round((Math.random() - 0.5) * 20 + data[i - 1]))
   }
+  const option = {
+    dataset: [],
+    xAxis: {
+      type: 'category',
+      nameLocation: 'middle',
+      data: date
+    },
+    yAxis: {},
+    tooltip: { trigger: 'axis' },
+    series: {
+      type: 'line',
+      symbol: 'none',
+      itemStyle: {
+        color: 'rgb(255, 70, 131)'
+      },
+      sampling: 'lttb',
+      areaStyle: {
+        color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+          {
+            offset: 0,
+            color: 'rgb(255, 158, 68)'
+          },
+          {
+            offset: 1,
+            color: 'rgb(255, 70, 131)'
+          }
+        ])
+      },
+      data
+    }
+  }
+
   myChart.setOption(option)
 })
 </script>
